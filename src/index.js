36f2d7c312e8b6ecfc0a3a55f18e02a150c53e6f
Matthew Sommers <mgsommers@mintz.com>
2020-04-04 14:37:45 -0400

    Walk through tutorial. Interesting stuff.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Typography from '@material-ui/core/Typography';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+import useTodoState from './useTodoState';
 import './styles.css';
 
 const App = () => {
+  const { todos, addTodo, deleteTodo } = useTodoState([]);
+
   return (
     <div className="App">
       <Typography component="h1" variant="h2">
         Todos
       </Typography>
+      <TodoForm saveTodo={(todoText) => {
+          const trimmedText = todoText.trim();
+
+          if (trimmedText.length > 0) {
+              addTodo(trimmedText)
+          }
+      }}
+      />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
     </div>
   );
 };
